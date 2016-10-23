52747c9403bc23e0f21cee36b242fa08c524f776
ngocnhan2205 <therockit1993@gmail.com>
2016-10-23 13:38:25 +0700

    update code

--- src/main/java/com/n2/portal/configuration/database/HibernateConfiguration.java
+++ src/main/java/com/n2/portal/configuration/database/HibernateConfiguration.java
@@ -3,14 +3,12 @@ package com.n2.portal.configuration.database;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
