0f699280a254e15f15d0a46ab6517236458acda9
ngocnhan2205 <ngocnhan2205@gmail.com>
2017-01-01 21:51:17 +0700

    update code

--- src/main/webapp/n2/js/expense/controllers/spend.controller.js
+++ src/main/webapp/n2/js/expense/controllers/spend.controller.js
@@ -1,7 +1,11 @@
 /**
  * Created by banhu on 1/1/2017.
  */
-expenseModule.controller('DialogSpendController', ['$scope', 'DATE',
-    function ($scope, DATE) {
+expenseModule.controller('DialogSpendController', ['$scope', '$mdDialog', 'DATE',
+    function ($scope, $mdDialog, DATE) {
         $scope.date = DATE;
+
+        $scope.close = function () {
+            $mdDialog.hide();
+        }
     }]);
